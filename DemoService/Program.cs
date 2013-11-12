5721cdf8ab8b9854e71adc49b07f4ee8599066f9
stulange <stuart.lange@gmail.com>
2013-11-11 21:23:21 -0600

    How to Create a Windows Service
    http://www.devopsonwindows.com/create-a-windows-service/

---
+++ DemoService/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoService
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ServiceBase.Run(new MyService());
+        }
+    }
+
+    class MyService : ServiceBase
+    {
+    }
+}
