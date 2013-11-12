33bc7b1b60d1223c7f8f78c9e3eb74745c09330d
stulange <stuart.lange@gmail.com>
2013-11-11 21:32:58 -0600

    How to install a windows service
    Use installutil to install a windows servcie
    
    http://www.devopsonwindows.com/install-a-windows-service/

--- DemoService/Program.cs
+++ DemoService/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration.Install;
 using System.Linq;
 using System.ServiceProcess;
 using System.Text;
@@ -17,5 +19,28 @@ namespace DemoService
 
     class MyService : ServiceBase
     {
+        public const string Name = "DemoService";
+        public MyService()
+        {
+            ServiceName = Name;
+        }
+    }
+
+    [RunInstaller(true)]
+    public class MyInstaller : Installer
+    {
+        public MyInstaller()
+        {
+            Installers.Add(new ServiceProcessInstaller
+            {
+                Account = ServiceAccount.LocalSystem
+            });
+            Installers.Add(new ServiceInstaller
+                {
+                    ServiceName = MyService.Name,
+                    DisplayName = MyService.Name,
+                    Description = "This is a demo service"
+                });
+        }
     }
 }
